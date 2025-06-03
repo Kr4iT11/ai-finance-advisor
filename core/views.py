fed41f59b956788f3bcea423808555e968b3ce98
Kr4iT11 <gurusingh463@gmail.com>
2025-06-03 14:50:28 +0530

    added token changes

--- core/views.py
+++ core/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from rest_framework import generics, permissions
+from .models import Transaction
+from .serializers import TransactionSerializer
 # Create your views here.
+
+class TransactionListCreateView(generics.ListCreateAPIView):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+    
+    def get_queryset(self):
+        return Transaction.objects.filter(user=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
